89291f87f57337c73dbf79364cd857cf0db7e36d
vectorx <uxiahnan@outlook.com>
2024-01-12 20:23:14 +0800

    feat: 尚医通平台前端搭建
    Nuxt环境初始化

---
+++ yygh-site/plugins/myPlugin.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+//element-ui的全部组件
+import ElementUI from "element-ui";
+//element-ui的css
+import "element-ui/lib/theme-chalk/index.css";
+
+//使用elementUI
+Vue.use(ElementUI);
